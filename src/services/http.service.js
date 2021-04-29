6dd3937268aa242679778af9d281f8781c0aceec
Daniel Keva <danielk@webiya.co.il>
2021-04-29 11:03:21 +0300

    added SERVER_URL

--- src/services/http.service.js
+++ src/services/http.service.js
@@ -1,39 +1,34 @@
-
 import Axios from 'axios';
-const BASE_URL = process.env.NODE_ENV === 'production'
-    ? '/api/'
-    : '//localhost:3030/api/'
-
+const { REACT_APP_SERVER_URL = '//localhost:3030' } = process.env;
 
 var axios = Axios.create({
-    withCredentials: true
+    withCredentials: true,
 });
 
 export default {
     get(endpoint, data) {
-        return ajax(endpoint, 'GET', data)
+        return ajax(endpoint, 'GET', data);
     },
     post(endpoint, data) {
-        return ajax(endpoint, 'POST', data)
+        return ajax(endpoint, 'POST', data);
     },
     put(endpoint, data) {
-        return ajax(endpoint, 'PUT', data)
+        return ajax(endpoint, 'PUT', data);
     },
     delete(endpoint, data) {
-        return ajax(endpoint, 'DELETE', data)
-    }
-}
+        return ajax(endpoint, 'DELETE', data);
+    },
+};
 
 async function ajax(endpoint, method = 'get', data = null) {
     try {
         const res = await axios({
-            url: `${BASE_URL}${endpoint}`,
+            url: `${REACT_APP_SERVER_URL}${endpoint}`,
             method,
-            data
-        })
+            data,
+        });
         return res.data;
     } catch (err) {
         throw err;
     }
 }
-
